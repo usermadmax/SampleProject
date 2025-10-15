79ff64147365859d91dbdbf1ab8fc0a4e5e1cfbb
GaneshKumar Aenagandula <ganeshaenagandula2005@gmail.com>
2025-10-15 23:47:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Car car = new Car("Toyota", 4, 4);
         Bike bike = new Bike("Yamaha", 2, true);
+        Engine engine = new Engine(500);
+        Truck truck = new Truck("Volvo", 6, engine);
+        ElectricCar eCar = new ElectricCar("Tesla", 4, 4, 100);
 
         Garage garage = new Garage();
         garage.addVehicle(car);
         garage.addVehicle(bike);
-        garage.listVehicles();
+        garage.addVehicle(truck);
+        garage.addVehicle(eCar);
 
+        ArrayList<Vehicle> vehicleList = new ArrayList<>();
+        vehicleList.add(car);
+        vehicleList.add(bike);
+        vehicleList.add(truck);
+        vehicleList.add(eCar);
+
+        Mechanic mechanic = new Mechanic();
+        mechanic.serviceVehicles(vehicleList);
+
+        eCar.recharge();
         car.honk();
         bike.kickStart();
+        truck.startTruck();
     }
 }
